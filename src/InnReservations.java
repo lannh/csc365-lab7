2629b99921c670fd0d14168e162479e5373475dc
Lan Nguyen <lannguyen@Lans-MacBook-Pro.local>
2022-12-02 10:39:44 -0800

    initial commit

---
+++ src/InnReservations.java
@@ -0,0 +1,52 @@
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.LinkedHashMap;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
+
+public class InnReservations {
+
+    public static void main(String[] args) {
+        try {
+            InnReservations ir = new InnReservations();
+            int demoNum = Integer.parseInt(args[0]);
+
+            switch (demoNum) {
+                case 1: ir.R1(); break;
+//                case 2: hp.demo2(); break;
+//                case 3: hp.demo3(); break;
+//                case 4: hp.demo4(); break;
+//                case 5: hp.demo5(); break;
+            }
+
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+        } catch (Exception e2) {
+            System.err.println("Exception: " + e2.getMessage());
+        }
+    }
+
+    private void R1() throws SQLException {
+
+        try (Connection conn = DriverManager.getConnection(System.getenv("HP_JDBC_URL"),
+                System.getenv("HP_JDBC_USER"),
+                System.getenv("HP_JDBC_PW"))) {
+            // Step 2: Construct SQL statement
+            String sql = "SELECT * FROM yehlsing.lab7_rooms JOIN yehlaing.lab7_reservations;";
+
+            try (Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery(sql)) {
+
+            }
+
+
+        }
+}
