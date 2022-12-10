327d5139756603039785c7069e5d97951639ee7c
xhwallace <xhw2000@gmail.com>
2022-12-09 17:38:40 -0800

    Add R1 functionality

--- src/InnReservations.java
+++ src/InnReservations.java
@@ -40,13 +40,71 @@ public class InnReservations {
                 System.getenv("HP_JDBC_USER"),
                 System.getenv("HP_JDBC_PW"))) {
             // Step 2: Construct SQL statement
-            String sql = "SELECT * FROM yehlsing.lab7_rooms JOIN yehlaing.lab7_reservations;";
+            String sql = "select roomname, popScore,nextAvail , MostRecentLength\n" +
+                    "from\n" +
+                    "(select rooms1.roomcode, ROUND(COALESCE(sum(stayTime), 0)/180 , 2)as popScore\n" +
+                    "    from\n" +
+                    "        yehlaing.lab7_rooms as rooms1 left join\n" +
+                    "        (SELECT \n" +
+                    "        roomcode, DATEDIFF(\n" +
+                    "            IF(DATEDIFF(CURDATE(), checkout) >= 0, checkout, CURDATE()),\n" +
+                    "            IF(DATEDIFF(CURDATE(), checkin) < 180, checkin, DATE_ADD(CURDATE(), INTERVAL -180 DAY)))as stayTime, \n" +
+                    "        DATEDIFF(CURDATE(),checkout), checkin,checkout\n" +
+                    "        from yehlaing.lab7_rooms\n" +
+                    "            join yehlaing.lab7_reservations on roomcode= room\n" +
+                    "        where (DATEDIFF(CURDATE(),checkout)< 180 and DATEDIFF(CURDATE(),checkout) >= 0 )\n" +
+                    "        and   (DATEDIFF(CURDATE(),checkin) < 180 and DATEDIFF(CURDATE(),checkin) >= 0 ))as t1\n" +
+                    "        on rooms1.roomcode = t1.roomcode\n" +
+                    "    group by roomcode) as A\n" +
+                    "join\n" +
+                    "    (select B1.roomcode, \n" +
+                    "        (SELECT\n" +
+                    "        CASE\n" +
+                    "            WHEN (NOT EXISTS \n" +
+                    "                (SELECT *\n" +
+                    "                FROM yehlaing.lab7_rooms join yehlaing.lab7_reservations on roomcode= room\n" +
+                    "                WHERE roomcode = B1.roomcode and CURDATE() >= checkin and CURDATE() < checkout))\n" +
+                    "            THEN\n" +
+                    "                CURDATE()\n" +
+                    "            ELSE \n" +
+                    "            (select min(checkout)\n" +
+                    "            from  yehlaing.lab7_reservations as RES_O\n" +
+                    "            where RES_O.room = B1.roomcode and RES_O.checkout > CURDATE() \n" +
+                    "                and RES_O.code NOT IN\n" +
+                    "                    (select RES1.code\n" +
+                    "                    from     yehlaing.lab7_reservations as RES1 \n" +
+                    "                        join yehlaing.lab7_reservations as RES2 \n" +
+                    "                        on RES1.room= RES2.room and RES1.checkout = RES2.checkin))\n" +
+                    "        END) as nextAvail\n" +
+                    "    from yehlaing.lab7_rooms as B1) as B\n" +
+                    "on A.roomcode = B.roomcode\n" +
+                    "join\n" +
+                    "    (SELECT roomcode, roomname, checkout, DATEDIFF(checkout, checkin) as MostRecentLength\n" +
+                    "    from yehlaing.lab7_rooms as rooms1 join yehlaing.lab7_reservations as res1 on roomcode= room\n" +
+                    "    where checkout <CURDATE() and checkout = \n" +
+                    "        (SELECT max(checkout) from\n" +
+                    "            (SELECT roomcode, roomname, checkout, DATEDIFF(checkout, checkin) as roomdiff\n" +
+                    "            from yehlaing.lab7_rooms join yehlaing.lab7_reservations on roomcode= room\n" +
+                    "            where checkout <CURDATE()) as C1\n" +
+                    "        where C1.roomcode = rooms1.roomcode\n" +
+                    "        group by roomcode)\n" +
+                    "    ) as C\n" +
+                    "on A.roomcode = C.roomcode\n" +
+                    "order by popScore desc";
 
             try (Statement stmt = conn.createStatement();
                  ResultSet rs = stmt.executeQuery(sql)) {
+                while(rs.next()){
+                    String roomcode = rs.getString("roomname");
+                    String popScore = rs.getString("popScore");
+                    String nextAvail = rs.getString("nextAvail");
+                    String mostRecentLength = rs.getString("MostRecentLength");
 
+
+                    System.out.format("%-30s %-10s %-15s %-10s\n", roomcode, popScore, nextAvail, mostRecentLength);
+
+                }
             }
-
-
+            }
         }
 }
